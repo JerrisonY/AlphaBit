32abcd990b54d88470ec9909a4f61e95b5ba669f
Jerrison Yip <jerryyip24@gmail.com>
2023-10-28 12:48:23 -0400

    starting files

---
+++ src/components/App/App.jsx
@@ -0,0 +1,19 @@
+import 'normalize.css'
+import './App.css'
+import { Routes, Route } from 'react-router-dom'
+import LandingPage from '../../pages/LandingPage/LandingPage'
+
+
+
+function App() {
+
+  return (
+    <main>
+      <Routes>
+        <Route path='/' element={LandingPage}/>
+      </Routes>
+    </main>
+  )
+}
+
+export default App
