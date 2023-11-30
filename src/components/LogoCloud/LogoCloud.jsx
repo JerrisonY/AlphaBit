1ecf8a8759e6c58f135410ed7b53c8ca82b72db5
Jerrison Yip <jerryyip24@gmail.com>
2023-11-29 23:46:36 -0500

    changed logos

--- src/components/LogoCloud/LogoCloud.jsx
+++ src/components/LogoCloud/LogoCloud.jsx
@@ -7,36 +7,36 @@ export default function Example() {
           </h2>
           <div className="mx-auto mt-10 grid max-w-lg grid-cols-4 items-center gap-x-8 gap-y-10 sm:max-w-xl sm:grid-cols-6 sm:gap-x-10 lg:mx-0 lg:max-w-none lg:grid-cols-5">
             <img
-              className="col-span-2 max-h-12 w-full object-contain lg:col-span-1"
-              src="https://tailwindui.com/img/logos/158x48/transistor-logo-white.svg"
+              className="col-span-2 max-h-14 w-full object-contain lg:col-span-1"
+              src="/logos/clg.png"
               alt="Transistor"
               width={158}
               height={48}
             />
             <img
               className="col-span-2 max-h-12 w-full object-contain lg:col-span-1"
-              src="https://tailwindui.com/img/logos/158x48/reform-logo-white.svg"
+              src="/logos/tsm.png"
               alt="Reform"
               width={158}
               height={48}
             />
             <img
               className="col-span-2 max-h-12 w-full object-contain lg:col-span-1"
-              src="https://tailwindui.com/img/logos/158x48/tuple-logo-white.svg"
+              src="/logos/teamliquid.png"
               alt="Tuple"
               width={158}
               height={48}
             />
             <img
               className="col-span-2 max-h-12 w-full object-contain sm:col-start-2 lg:col-span-1"
-              src="https://tailwindui.com/img/logos/158x48/savvycal-logo-white.svg"
+              src="/logos/100thieves.png"
               alt="SavvyCal"
               width={158}
               height={48}
             />
             <img
               className="col-span-2 col-start-2 max-h-12 w-full object-contain sm:col-start-auto lg:col-span-1"
-              src="https://tailwindui.com/img/logos/158x48/statamic-logo-white.svg"
+              src="/logos/cloud9.png"
               alt="Statamic"
               width={158}
               height={48}
