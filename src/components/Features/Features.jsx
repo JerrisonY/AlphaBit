97c8f16b804e87ec56802a759d6a4e2aa8d7248b
Jerrison Yip <jerryyip24@gmail.com>
2023-12-01 13:00:30 -0500

    .

--- src/components/Features/Features.jsx
+++ src/components/Features/Features.jsx
@@ -45,7 +45,8 @@ export default function Example() {
             </div>
           </div>
           <img
-            src="https://tailwindui.com/img/component-images/dark-project-app-screenshot.png"
+            // src="https://tailwindui.com/img/component-images/dark-project-app-screenshot.png"
+            src="/images/test.png"
             alt="Product screenshot"
             className="w-[48rem] max-w-none rounded-xl shadow-xl ring-1 ring-gray-400/10 sm:w-[57rem] md:-ml-4 lg:-ml-0"
             width={2432}
